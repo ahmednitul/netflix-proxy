13392d39dfa451250bce78cdd0c0de98bfe9ee1a
Ahmed Nitul <ahmednitul@gmail.com>
2017-02-12 11:29:20 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -215,12 +215,10 @@ sudo iptables -t nat -A PREROUTING -i ${IFACE} -p tcp --dport 80 -j REDIRECT --t
   sudo iptables -A INPUT -p icmp -j ACCEPT && \
   sudo iptables -A INPUT -i lo -j ACCEPT && \
   sudo iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT && \
-  sudo iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 81 -j ACCEPT && \
   sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT && \
   sudo iptables -A INPUT -p udp -m udp --dport 53 -j ACCEPT && \
   sudo iptables -A INPUT -p udp -m udp --dport 5353 -j ACCEPT && \
   sudo iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT && \
-  sudo iptables -A INPUT -p tcp -m tcp --dport 88 -j ACCEPT && \
   sudo iptables -A INPUT -p tcp -m tcp --dport 8080 -j ACCEPT && \
   sudo iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT && \
   sudo iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
